eaa6b7d69aee77f2e672f19376156a71626cc0b6
SergiBoadas <sergiboadas@gmail.com>
2018-03-24 12:46:37 +0100

    Creada l'actiitat getUser buida

---
+++ app/src/main/java/org/udg/pds/cheapyandroid/activity/GetUser.java
@@ -0,0 +1,11 @@
+package org.udg.pds.cheapyandroid.activity;
+
+import android.app.Activity;
+import org.udg.pds.cheapyandroid.rest.CheapyApi;
+
+public class GetUser extends Activity {
+
+    CheapyApi mTodoService;
+
+
+}
