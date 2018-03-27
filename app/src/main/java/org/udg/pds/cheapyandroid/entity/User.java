24ba7c24c47b24b43f18c51b3c2fe96d6a599bba
SergiBoadas <sergiboadas@gmail.com>
2018-03-27 17:48:14 +0200

    Implementada la classe User per tal de poder llegir les dades corresponents dels usuaris del servidor

--- app/src/main/java/org/udg/pds/cheapyandroid/entity/User.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/entity/User.java
@@ -1,4 +1,43 @@
 package org.udg.pds.cheapyandroid.entity;
 
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "usuari"
+})
 public class User {
+
+    @JsonProperty("usuari")
+    private User usuari;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("usuari")
+    public User getUsuari() {
+        return usuari;
+    }
+
+    @JsonProperty("usuari")
+    public void setUsuari(User usuari) {
+        this.usuari = usuari;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
