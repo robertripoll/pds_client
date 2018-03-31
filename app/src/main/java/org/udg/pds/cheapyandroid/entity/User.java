c15e545150f44e17adc7f5848970c6695ca9f654
luisleon1894 <luisleon1894@gmail.com>
2018-03-31 11:15:29 +0200

    S'han afegit atributs per utilitzar alhora de donar d'alta

--- app/src/main/java/org/udg/pds/cheapyandroid/entity/User.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/entity/User.java
@@ -4,4 +4,10 @@ public class User {
     public long id;
     public String correu;
     public String contrasenya;
+    public String sexe;
+    public String nom;
+    public String cognom;
+    public String dataNaixament;
+    public double telefon;
+
 }
