409575c54d6f595f0800c4595ba9f8d353c34657
luisleon1894 <luisleon1894@gmail.com>
2018-04-01 13:12:34 +0200

    S'ha afegit "/" al final de URL

--- app/src/main/java/org/udg/pds/cheapyandroid/util/Global.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/util/Global.java
@@ -2,7 +2,7 @@ package org.udg.pds.cheapyandroid.util;
 
 public class Global {
 
-    public static final String BASE_URL = "http://private-875f4-pdsrest.apiary-mock.com";
+    public static final String BASE_URL = "http://private-875f4-pdsrest.apiary-mock.com/";
 
     // Resquest codes for startActivityForResult
     public static final int RQ_ADD_TASK = 1;
