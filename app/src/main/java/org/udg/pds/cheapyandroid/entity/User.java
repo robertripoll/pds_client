b8d18dd7b4e560d25d8f92df38e70e321238b189
SergiBoadas <sergiboadas@gmail.com>
2018-04-03 21:46:03 +0200

    Reescrivida la classe

--- app/src/main/java/org/udg/pds/cheapyandroid/entity/User.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/entity/User.java
@@ -1,81 +1,6 @@
 package org.udg.pds.cheapyandroid.entity;
 
-
-public class User {
-
-    private String id;
-    private String nom;
-    private String cognoms;
-    private String dataNaixement;
-    private String correu;
-    private int nombreVendes;
-    private int nombreCompres;
-    private int nombreValoracions;
-    private double mitjanaValoracions;
-    private Ubicacio ubicació;
-    private String imatge;
-
-    public User(String id, String nom, String cognoms, String dataNaixement, String correu, int nombreVendes, int nombreCompres, int nombreValoracions, double mitjanaValoracions, Ubicacio ubicació, String imatge) {
-        this.id = id;
-        this.nom = nom;
-        this.cognoms = cognoms;
-        this.dataNaixement = dataNaixement;
-        this.correu = correu;
-        this.nombreVendes = nombreVendes;
-        this.nombreCompres = nombreCompres;
-        this.nombreValoracions = nombreValoracions;
-        this.mitjanaValoracions = mitjanaValoracions;
-        this.ubicació = ubicació;
-        this.imatge = imatge;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public String getCognoms() {
-        return cognoms;
-    }
-
-    public String getDataNaixement() {
-        return dataNaixement;
-    }
-
-    public String getCorreu() {
-        return correu;
-    }
-
-    public int getNombreVendes() {
-        return nombreVendes;
-    }
-
-    public int getNombreCompres() {
-        return nombreCompres;
-    }
-
-    public int getValoracions() {
-        return nombreValoracions;
-    }
-
-    public double getMitjanaValoracions() {
-        return mitjanaValoracions;
-    }
-
-    public Ubicacio getUbicació() {
-        return ubicació;
-    }
-
-    public String getImatge() {
-        return imatge;
-    }
-}
-
-
-/*import java.util.HashMap;
+import java.util.HashMap;
 import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
@@ -86,23 +11,208 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-        "usuari"
+        "id",
+        "nom",
+        "cognoms",
+        "dataNaixement",
+        "correu",
+        "nombreVendes",
+        "nombreCompres",
+        "nombreValoracions",
+        "mitjanaValoracions",
+        "ubicacio",
+        "imatgeURL"
 })
 public class User {
 
-    @JsonProperty("usuari")
-    private User usuari;
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("nom")
+    private String nom;
+    @JsonProperty("cognoms")
+    private String cognoms;
+    @JsonProperty("dataNaixement")
+    private String dataNaixement;
+    @JsonProperty("correu")
+    private String correu;
+    @JsonProperty("nombreVendes")
+    private Integer nombreVendes;
+    @JsonProperty("nombreCompres")
+    private Integer nombreCompres;
+    @JsonProperty("nombreValoracions")
+    private Integer nombreValoracions;
+    @JsonProperty("mitjanaValoracions")
+    private Double mitjanaValoracions;
+    @JsonProperty("ubicacio")
+    private Ubicacio ubicacio;
+    @JsonProperty("imatgeURL")
+    private String imatgeURL;
     @JsonIgnore
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-    @JsonProperty("usuari")
-    public User getUsuari() {
-        return usuari;
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
     }
 
-    @JsonProperty("usuari")
-    public void setUsuari(User usuari) {
-        this.usuari = usuari;
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    @JsonProperty("nom")
+    public String getNom() {
+        return nom;
+    }
+
+    @JsonProperty("nom")
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public User withNom(String nom) {
+        this.nom = nom;
+        return this;
+    }
+
+    @JsonProperty("cognoms")
+    public String getCognoms() {
+        return cognoms;
+    }
+
+    @JsonProperty("cognoms")
+    public void setCognoms(String cognoms) {
+        this.cognoms = cognoms;
+    }
+
+    public User withCognoms(String cognoms) {
+        this.cognoms = cognoms;
+        return this;
+    }
+
+    @JsonProperty("dataNaixement")
+    public String getDataNaixement() {
+        return dataNaixement;
+    }
+
+    @JsonProperty("dataNaixement")
+    public void setDataNaixement(String dataNaixement) {
+        this.dataNaixement = dataNaixement;
+    }
+
+    public User withDataNaixement(String dataNaixement) {
+        this.dataNaixement = dataNaixement;
+        return this;
+    }
+
+    @JsonProperty("correu")
+    public String getCorreu() {
+        return correu;
+    }
+
+    @JsonProperty("correu")
+    public void setCorreu(String correu) {
+        this.correu = correu;
+    }
+
+    public User withCorreu(String correu) {
+        this.correu = correu;
+        return this;
+    }
+
+    @JsonProperty("nombreVendes")
+    public Integer getNombreVendes() {
+        return nombreVendes;
+    }
+
+    @JsonProperty("nombreVendes")
+    public void setNombreVendes(Integer nombreVendes) {
+        this.nombreVendes = nombreVendes;
+    }
+
+    public User withNombreVendes(Integer nombreVendes) {
+        this.nombreVendes = nombreVendes;
+        return this;
+    }
+
+    @JsonProperty("nombreCompres")
+    public Integer getNombreCompres() {
+        return nombreCompres;
+    }
+
+    @JsonProperty("nombreCompres")
+    public void setNombreCompres(Integer nombreCompres) {
+        this.nombreCompres = nombreCompres;
+    }
+
+    public User withNombreCompres(Integer nombreCompres) {
+        this.nombreCompres = nombreCompres;
+        return this;
+    }
+
+    @JsonProperty("nombreValoracions")
+    public Integer getNombreValoracions() {
+        return nombreValoracions;
+    }
+
+    @JsonProperty("nombreValoracions")
+    public void setNombreValoracions(Integer nombreValoracions) {
+        this.nombreValoracions = nombreValoracions;
+    }
+
+    public User withNombreValoracions(Integer nombreValoracions) {
+        this.nombreValoracions = nombreValoracions;
+        return this;
+    }
+
+    @JsonProperty("mitjanaValoracions")
+    public Double getMitjanaValoracions() {
+        return mitjanaValoracions;
+    }
+
+    @JsonProperty("mitjanaValoracions")
+    public void setMitjanaValoracions(Double mitjanaValoracions) {
+        this.mitjanaValoracions = mitjanaValoracions;
+    }
+
+    public User withMitjanaValoracions(Double mitjanaValoracions) {
+        this.mitjanaValoracions = mitjanaValoracions;
+        return this;
+    }
+
+    @JsonProperty("ubicacio")
+    public Ubicacio getUbicacio() {
+        return ubicacio;
+    }
+
+    @JsonProperty("ubicacio")
+    public void setUbicacio(Ubicacio ubicacio) {
+        this.ubicacio = ubicacio;
+    }
+
+    public User withUbicacio(Ubicacio ubicacio) {
+        this.ubicacio = ubicacio;
+        return this;
+    }
+
+    @JsonProperty("imatgeURL")
+    public String getImatgeURL() {
+        return imatgeURL;
+    }
+
+    @JsonProperty("imatgeURL")
+    public void setImatgeURL(String imatgeURL) {
+        this.imatgeURL = imatgeURL;
+    }
+
+    public User withImatgeURL(String imatgeURL) {
+        this.imatgeURL = imatgeURL;
+        return this;
     }
 
     @JsonAnyGetter
@@ -115,5 +225,9 @@ public class User {
         this.additionalProperties.put(name, value);
     }
 
+    public User withAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+        return this;
+    }
+
 }
-*/
