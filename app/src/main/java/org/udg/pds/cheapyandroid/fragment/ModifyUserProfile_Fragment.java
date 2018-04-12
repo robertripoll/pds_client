0b43756f4163ee2989a3639bfb4f1d321c434809
SergiBoadas <sergiboadas@gmail.com>
2018-04-12 17:33:55 +0200

    S'ha creat un nou fragment per poder modificar certes dades del usuari

---
+++ app/src/main/java/org/udg/pds/cheapyandroid/fragment/ModifyUserProfile_Fragment.java
@@ -0,0 +1,31 @@
+package org.udg.pds.cheapyandroid.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import org.udg.pds.cheapyandroid.CheapyApp;
+import org.udg.pds.cheapyandroid.R;
+import org.udg.pds.cheapyandroid.rest.CheapyApi;
+
+public class ModifyUserProfile_Fragment extends Fragment {
+
+    private CheapyApi mCheapyService;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.modificar_perfilusuari, container, false);
+        mCheapyService = ((CheapyApp) getActivity().getApplication()).getAPI();
+
+
+        return view;
+    }
+
+}
