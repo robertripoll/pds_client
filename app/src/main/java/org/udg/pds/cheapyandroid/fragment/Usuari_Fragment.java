802c892c3957014d4f540e530ac8bb31e980e9bd
SergiBoadas <sergiboadas@gmail.com>
2018-04-12 15:11:03 +0200

    Inicialitzada l'element del botonNavegation que em faltava i creat metode per escoltar els items del menu del usuari i afegides un parell d'IDs

--- app/src/main/java/org/udg/pds/cheapyandroid/fragment/Usuari_Fragment.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/fragment/Usuari_Fragment.java
@@ -4,7 +4,9 @@ package org.udg.pds.cheapyandroid.fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +14,8 @@ import android.view.ViewGroup;
 import org.udg.pds.cheapyandroid.CheapyApp;
 import org.udg.pds.cheapyandroid.R;
 import org.udg.pds.cheapyandroid.rest.CheapyApi;
+
+import java.util.Objects;
 
 public class Usuari_Fragment extends Fragment {
 
@@ -24,10 +28,10 @@ public class Usuari_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_usuari, container, false);
-
         mCheapyService = ((CheapyApp)getActivity().getApplication()).getAPI();
-
-        /*bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigation = (BottomNavigationView) view.findViewById(R.id.navigation);
+        canviarFragment(0);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
@@ -43,11 +47,10 @@ public class Usuari_Fragment extends Fragment {
                         //canviarColorMenu(bottomNavigation, 2);
                         canviarFragment(2);
                         break;
-
                 }
                 return true;
             }
-        });*/
+        });
 
         return view;
     }
@@ -55,17 +58,15 @@ public class Usuari_Fragment extends Fragment {
     private void canviarFragment(int i) {
         Fragment fragment = null;
         if (i == 0) {
+
             fragment = new PerfilUsuari_Fragment();
-            //} else if (i % 2 != 0) {
+        } //else if (i % 2 != 0) {
             //    fragment = new FragmentTwo();
             //} else {
             //    fragment = new FragmentThree();
             //}
 
-            getFragmentManager().beginTransaction().replace(
-                    R.id.fragment_usuari, fragment)
-                    .commit();
-        }
+       getFragmentManager().beginTransaction().add(R.id.fragment_usuari, fragment).commit();
     }
 
     private void canviarColorMenu(BottomNavigationView bottomNavigation, int i) {
