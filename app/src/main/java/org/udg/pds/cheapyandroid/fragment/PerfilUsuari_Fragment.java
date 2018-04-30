b5d6d23efe5c3676ff28e7b857fe3219563b5d6b
SergiBoadas <sergiboadas@gmail.com>
2018-04-30 18:41:12 +0200

    afegit un import que faltava a causa del merge

--- app/src/main/java/org/udg/pds/cheapyandroid/fragment/PerfilUsuari_Fragment.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/fragment/PerfilUsuari_Fragment.java
@@ -3,6 +3,7 @@ package org.udg.pds.cheapyandroid.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
--- app/src/main/java/org/udg/pds/cheapyandroid/rest/CheapyApi.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/rest/CheapyApi.java
@@ -2,9 +2,7 @@ package org.udg.pds.cheapyandroid.rest;
 
 import org.udg.pds.cheapyandroid.entity.*;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 public interface CheapyApi {
 
