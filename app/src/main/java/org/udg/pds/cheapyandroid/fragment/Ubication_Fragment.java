c00f411ce3566bbe1801a26a50f0e626e8049c30
SergiBoadas <sergiboadas@gmail.com>
2018-05-09 13:43:32 +0200

    creada la classe ubicacio_fragment a on en principi s'hauria de mostrar el punt ficat en el codi (punt trobat per internet com a exemple

--- app/src/main/java/org/udg/pds/cheapyandroid/fragment/Ubication_Fragment.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/fragment/Ubication_Fragment.java
@@ -1,21 +1,23 @@
 package org.udg.pds.cheapyandroid.fragment;
 
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import org.udg.pds.cheapyandroid.CheapyApp;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import org.udg.pds.cheapyandroid.R;
 import org.udg.pds.cheapyandroid.rest.CheapyApi;
-import com.google.android.gms.maps.OnMapReadyCallback;
-
 
 
 public class Ubication_Fragment extends Fragment implements OnMapReadyCallback{
     CheapyApi mCheapyService;
+    GoogleMap mygooglemap;
     MapView mapView;
     View myView;
 
@@ -24,10 +26,25 @@ public class Ubication_Fragment extends Fragment implements OnMapReadyCallback{
         myView = inflater.inflate(R.layout.fragment_ubication, container, false);
         return myView;
     }
-    
+
+    @Override
+    public void onViewCreated(View v, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(v,savedInstanceState);
+        mapView = (MapView) myView.findViewById(R.id.mapUser);
+        if(mapView != null){
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        MapsInitializer.initialize(getContext());
+        mygooglemap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247, -74.044502)).title("My position prova").snippet("alualu provant"));
+        CameraPosition alu = CameraPosition.builder().target(new LatLng(40.689247, -74.044502)).zoom(16).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(alu));
     }
 }
