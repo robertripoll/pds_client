8e2df7c13080545f4af3524af3fe2c2e6bd142f8
AlexSS <alexsegurasoler@gmail.com>
2018-05-16 14:06:49 +0200

    Update d'Editar Producte, En Progrés

--- app/src/main/java/org/udg/pds/cheapyandroid/fragment/EditarProductePerfilVenda.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/fragment/EditarProductePerfilVenda.java
@@ -2,163 +2,186 @@ package org.udg.pds.cheapyandroid.fragment;
 
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import org.udg.pds.cheapyandroid.entity.UserLogged;
+import android.widget.*;
+import org.udg.pds.cheapyandroid.CheapyApp;
+import org.udg.pds.cheapyandroid.R;
+import org.udg.pds.cheapyandroid.activity.LlistaProductesActivity;
+import org.udg.pds.cheapyandroid.entity.*;
 import org.udg.pds.cheapyandroid.rest.CheapyApi;
+import org.udg.pds.cheapyandroid.util.Global;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.List;
 
 public class EditarProductePerfilVenda extends Fragment {
 
     private CheapyApi mCheapyService;
-    private Button button_photo;
-    private Button button_update;
-    private ImageView photo;
-    private EditText preu;
-    private EditText descripcio;
-    private EditText categoria;
 
-    private boolean fotoActualitzada;
-    private UserLogged user;
+    private Spinner spinnerCategories;
+    private Button buttonPublicar;
+    private EditText editNomProducte;
+    private EditText editDescProducte;
+    private EditText editPreuProducte;
+    private CheckBox cbIntercanvi;
+    private CheckBox cbPreuNegociable;
+    private TextView tvError;
+
+    private List<Category> categories;
+
+    private String _user_id;
+
+
+    public EditarProductePerfilVenda() {
+        // Required empty public constructor
+    }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_modificar_perfilusuari, container, false);
-        mCheapyService = ((CheapyApp) getActivity().getApplication()).getAPI();
+        View view = inflater.inflate(R.layout.fragment_producte_editar, container, false);
 
-        inicialitzarVariables(view);
+        // Busquem els components de la vista.
+        spinnerCategories = (Spinner)view.findViewById(R.id.spinner_categoria);
+        buttonPublicar = (Button)view.findViewById(R.id.button_publicar);
+        editNomProducte = (EditText)view.findViewById(R.id.et_nom_producte);
+        editDescProducte = (EditText)view.findViewById(R.id.et_desc_producte);
+        editPreuProducte = (EditText)view.findViewById(R.id.et_preu_producte);
+        cbIntercanvi = (CheckBox)view.findViewById(R.id.cb_intercanvi);
+        cbPreuNegociable = (CheckBox)view.findViewById(R.id.cb_preu_negociable);
+        tvError = (TextView)view.findViewById(R.id.tv_error_falten_camps_publicar_anunci);
 
-        eventOnButtons();
+        // Llegeix l'usuari actual que hi ha a l'app
+        SharedPreferences prefs = getActivity().getSharedPreferences(Global.PREFS_NAME, Context.MODE_PRIVATE);
+        _user_id = prefs.getString("usuari_id", "0");
 
-        editPreuFocus(preu);
-        editTextFocus(descripcio);
-        editTextFocus(categoria);
+        // Posem l'error com a no visible.
+        tvError.setVisibility(View.GONE);
+
+        // Carraguem el servei i les categories.
+        mCheapyService = ((CheapyApp)getActivity().getApplication()).getAPI();
+        carregarCategories();
+
+        // Inicialitzem l'Spinner de categories.
+        crearOpcionsSpinnerCategories();
+
+        // Configurem l'acció del botó publicar.
+        buttonPublicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publicarAnunci();
+            }
+        });
 
         return view;
     }
 
-    private void editTextFocus(final EditText editText) {
-        editText.setOnFocusChangeListener(new View.OnFocusChangeListener(){
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                if (editText.getText().length()<1)
-                    editText.setError("No pots deixar els camps buits!");
-            }
-        });
-    }
+    private void publicarAnunci() {
+        String nom = editNomProducte.getText().toString();
+        String desc = editDescProducte.getText().toString();
+        String preuTxt = editPreuProducte.getText().toString();
+        Integer iCategoria = spinnerCategories.getSelectedItemPosition();
+        Boolean intercanvi = cbIntercanvi.isChecked();
+        Boolean negociable = cbPreuNegociable.isChecked();
 
-    private void editPreuFocus(final EditText preu) {
-        preu.setOnFocusChangeListener(new View.OnFocusChangeListener(){
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                if(preu.getText().length()<0){
-                    preu.setError("No pots posar un preu negatiu!");
-                }
-            }
-        });
-    }
-
-    private void eventOnButtons() {
-        button_update.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                if(comprovarSiInfoPlena()){
-                    mostrarMenuConfirmacio();
-                }
-                else{
-                    Toast.makeText(getContext(),"ERROR: Missing Required Fields",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        button_photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mostrarOpcions();
-            }
-        });
-    }
-
-    private void mostrarMenuConfirmacio() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext());
-        alertDialog.setTitle("Are you sure that you have filled in the fields correctly?");
-        alertDialog.setPositiveButton("I'm sure!", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                updateInformation();
-            }
-        });
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                preu.setText("");
-                categoria.setText("");
-                descripcio.setText("");
-                dialogInterface.dismiss();
-            }
-        });
-        alertDialog.show();
-    }
-
-    private void updateInformation() {
-        PerfilUsuari_Fragment fragmentPerf = new PerfilUsuari_Fragment();
-        Bundle bundle = new Bundle();
-        userAct.setNom(String.valueOf(nom.getText()));
-        userAct.setCognoms(String.valueOf(cognom.getText()));
-        userAct.setTelefon(String.valueOf(telefon.getText()));
-        bundle.putSerializable("newUser",userAct);
-        fragmentPerf.setArguments(bundle);
-        getFragmentManager().beginTransaction().replace(R.id.fragment_modificarPerfil, fragmentPerf).commit();
-    }
-
-    private Boolean comprovarSiInfoPlena() {
-        return nom.getText().length()>=3 && cognom.getText().length()>=3
-                && telefon.getText().length()>=9;
-    }
-
-    private void inicialitzarVariables(View view) {
-        button_photo = (Button) view.findViewById(R.id.botoFotoActualitzada);
-        button_update = (Button) view.findViewById(R.id.botoActualitzarInformacio);
-        photo = (ImageView) view.findViewById(R.id.fotoActualitzada);
-        fotoActualitzada = false;
-        Bundle bundle = getArguments();
-    }
-
-    private void mostrarOpcions() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setTitle("Choose an action");
-        final String opcio1 = "Choose a photo";
-        final String opcio2 = "Cancel";
-        final CharSequence [] diversesOpcions = {opcio1,opcio2};
-        builder.setItems(diversesOpcions, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if(diversesOpcions[i].equals(opcio1)){
-                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    intent.setType("image/");
-                    startActivityForResult(intent.createChooser(intent,"Choose"),CODI_SELECCIO);
-                }
-                else{
-                    dialogInterface.dismiss();
-                }
-            }
-        });
-        builder.show();
-    }
-
-    //Com que hem utilitzat el startActivityForResults, hem de sobrescriure el següent mètode
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CODI_SELECCIO){
-            Uri elMeuPath = data.getData();
-            photo.setImageURI(elMeuPath);
-            fotoActualitzada = true;
+        if (stringNullOrEmpty(nom) || stringNullOrEmpty(desc) || stringNullOrEmpty(preuTxt) || iCategoria == 0) {
+            tvError.setVisibility(View.VISIBLE);
         }
+        else {
+            tvError.setVisibility(View.GONE);
+            iCategoria = iCategoria - 1; // A la primera posició hi ha el "Selecciona una categoria...".
+
+            // No hi ha error, creem el producte i fem el POST.
+            Producte producte  = new Producte();
+            producte.setNom(nom);
+            producte.setDescripcio(desc);
+            producte.setPreu(Double.parseDouble(preuTxt));
+            producte.setCategoria(categories.get(iCategoria).getCategoria());
+            producte.setPreuNegociable(negociable);
+            producte.setIntercanviAcceptat(intercanvi);
+
+            Venedor venedor = new Venedor();
+            venedor.setId(Integer.parseInt(_user_id));
+            producte.setVenedor(venedor);
+
+            // Fem el POST.
+            postAnunciProducte(producte);
+        }
+    }
+
+    private void postAnunciProducte(Producte producte) {
+        Call<Void> call = mCheapyService.crearProducte(producte);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    Toast toast = Toast.makeText(getActivity(), "Anunci enviat correctament.", Toast.LENGTH_SHORT);
+                    toast.show();
+
+                    LlistaProductesActivity activity = (LlistaProductesActivity)getActivity();
+                    activity.carregarProductesALaVenda();
+
+                } else {
+                    Toast toast = Toast.makeText(getActivity(), "Error enviant anunci.", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast toast = Toast.makeText(getActivity(), "ERROR: Revisa la connexió a Internet.", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+    private Boolean stringNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+
+    private void crearOpcionsSpinnerCategories() {
+        ArrayAdapter<String> adapterCategories = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
+        adapterCategories.add("Selecciona una Categoria...");
+
+        if (categories != null) {
+            for (Category categoria : categories) {
+                adapterCategories.add(categoria.getCategoria().getNom());
+            }
+        }
+
+        spinnerCategories.setAdapter(adapterCategories);
+    }
+
+    private void carregarCategories() {
+        Call<LlistaCategories> call = mCheapyService.getCategories();
+        call.enqueue(new Callback<LlistaCategories>() {
+            @Override
+            public void onResponse(Call<LlistaCategories> call, Response<LlistaCategories> response) {
+
+                if (response.isSuccessful()) {
+                    LlistaCategories llista = response.body();
+                    categories = llista.getCategories();
+                    crearOpcionsSpinnerCategories();
+                } else {
+                    Toast toast = Toast.makeText(getActivity(), "ERROR: Les categories no s'han pogut carregar correctament", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LlistaCategories> call, Throwable t) {
+                Toast toast = Toast.makeText(getActivity(), "ERROR: Revisa la connexió a Internet.", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 
 }
