e24c381fd32a09578eb03db0850d9c386e86945e
SergiBoadas <sergiboadas@gmail.com>
2018-05-20 14:06:39 +0200

    refactor de la classe degut al canvi de lapiary degut a les imatges

--- app/src/main/java/org/udg/pds/cheapyandroid/entity/UserLogged.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/entity/UserLogged.java
@@ -1,6 +1,5 @@
 package org.udg.pds.cheapyandroid.entity;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
@@ -20,12 +19,13 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
         "telefon",
         "dataNaix",
         "ubicacio",
+        "imatge",
         "nombreValoracions",
         "mitjanaValoracions",
         "nombreCompres",
         "nombreVendes"
 })
-public class UserLogged implements Serializable{
+public class UserLogged {
 
     @JsonProperty("id")
     private Integer id;
@@ -43,6 +43,8 @@ public class UserLogged implements Serializable{
     private String dataNaix;
     @JsonProperty("ubicacio")
     private Ubicacio ubicacio;
+    @JsonProperty("imatge")
+    private Object imatge;
     @JsonProperty("nombreValoracions")
     private Integer nombreValoracions;
     @JsonProperty("mitjanaValoracions")
@@ -64,6 +66,11 @@ public class UserLogged implements Serializable{
         this.id = id;
     }
 
+    public UserLogged withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
     @JsonProperty("correu")
     public String getCorreu() {
         return correu;
@@ -72,6 +79,11 @@ public class UserLogged implements Serializable{
     @JsonProperty("correu")
     public void setCorreu(String correu) {
         this.correu = correu;
+    }
+
+    public UserLogged withCorreu(String correu) {
+        this.correu = correu;
+        return this;
     }
 
     @JsonProperty("nom")
@@ -84,6 +96,11 @@ public class UserLogged implements Serializable{
         this.nom = nom;
     }
 
+    public UserLogged withNom(String nom) {
+        this.nom = nom;
+        return this;
+    }
+
     @JsonProperty("cognoms")
     public String getCognoms() {
         return cognoms;
@@ -92,6 +109,11 @@ public class UserLogged implements Serializable{
     @JsonProperty("cognoms")
     public void setCognoms(String cognoms) {
         this.cognoms = cognoms;
+    }
+
+    public UserLogged withCognoms(String cognoms) {
+        this.cognoms = cognoms;
+        return this;
     }
 
     @JsonProperty("sexe")
@@ -104,6 +126,11 @@ public class UserLogged implements Serializable{
         this.sexe = sexe;
     }
 
+    public UserLogged withSexe(String sexe) {
+        this.sexe = sexe;
+        return this;
+    }
+
     @JsonProperty("telefon")
     public String getTelefon() {
         return telefon;
@@ -112,6 +139,11 @@ public class UserLogged implements Serializable{
     @JsonProperty("telefon")
     public void setTelefon(String telefon) {
         this.telefon = telefon;
+    }
+
+    public UserLogged withTelefon(String telefon) {
+        this.telefon = telefon;
+        return this;
     }
 
     @JsonProperty("dataNaix")
@@ -124,6 +156,11 @@ public class UserLogged implements Serializable{
         this.dataNaix = dataNaix;
     }
 
+    public UserLogged withDataNaix(String dataNaix) {
+        this.dataNaix = dataNaix;
+        return this;
+    }
+
     @JsonProperty("ubicacio")
     public Ubicacio getUbicacio() {
         return ubicacio;
@@ -132,6 +169,26 @@ public class UserLogged implements Serializable{
     @JsonProperty("ubicacio")
     public void setUbicacio(Ubicacio ubicacio) {
         this.ubicacio = ubicacio;
+    }
+
+    public UserLogged withUbicacio(Ubicacio ubicacio) {
+        this.ubicacio = ubicacio;
+        return this;
+    }
+
+    @JsonProperty("imatge")
+    public Object getImatge() {
+        return imatge;
+    }
+
+    @JsonProperty("imatge")
+    public void setImatge(Object imatge) {
+        this.imatge = imatge;
+    }
+
+    public UserLogged withImatge(Object imatge) {
+        this.imatge = imatge;
+        return this;
     }
 
     @JsonProperty("nombreValoracions")
@@ -144,6 +201,11 @@ public class UserLogged implements Serializable{
         this.nombreValoracions = nombreValoracions;
     }
 
+    public UserLogged withNombreValoracions(Integer nombreValoracions) {
+        this.nombreValoracions = nombreValoracions;
+        return this;
+    }
+
     @JsonProperty("mitjanaValoracions")
     public Object getMitjanaValoracions() {
         return mitjanaValoracions;
@@ -152,6 +214,11 @@ public class UserLogged implements Serializable{
     @JsonProperty("mitjanaValoracions")
     public void setMitjanaValoracions(Object mitjanaValoracions) {
         this.mitjanaValoracions = mitjanaValoracions;
+    }
+
+    public UserLogged withMitjanaValoracions(Object mitjanaValoracions) {
+        this.mitjanaValoracions = mitjanaValoracions;
+        return this;
     }
 
     @JsonProperty("nombreCompres")
@@ -164,6 +231,11 @@ public class UserLogged implements Serializable{
         this.nombreCompres = nombreCompres;
     }
 
+    public UserLogged withNombreCompres(Integer nombreCompres) {
+        this.nombreCompres = nombreCompres;
+        return this;
+    }
+
     @JsonProperty("nombreVendes")
     public Integer getNombreVendes() {
         return nombreVendes;
@@ -174,6 +246,11 @@ public class UserLogged implements Serializable{
         this.nombreVendes = nombreVendes;
     }
 
+    public UserLogged withNombreVendes(Integer nombreVendes) {
+        this.nombreVendes = nombreVendes;
+        return this;
+    }
+
     @JsonAnyGetter
     public Map<String, Object> getAdditionalProperties() {
         return this.additionalProperties;
@@ -182,6 +259,11 @@ public class UserLogged implements Serializable{
     @JsonAnySetter
     public void setAdditionalProperty(String name, Object value) {
         this.additionalProperties.put(name, value);
+    }
+
+    public UserLogged withAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+        return this;
     }
 
 }
