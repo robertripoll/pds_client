8af44c9624c2b02a2f207155b5bc45caf20eea65
SergiBoadas <sergiboadas@gmail.com>
2018-06-02 19:57:28 +0200

    modificat tot lo necessari per tal de que la splash screen funcioni com a prova

--- app/src/main/java/org/udg/pds/cheapyandroid/activity/SplashScreen.java
+++ app/src/main/java/org/udg/pds/cheapyandroid/activity/SplashScreen.java
@@ -1,12 +1,40 @@
 package org.udg.pds.cheapyandroid.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.udg.pds.cheapyandroid.R;
 
 public class SplashScreen extends AppCompatActivity {
-
+    private ImageView imagesplash;
+    private TextView textsplash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        textsplash = (TextView) findViewById(R.id.textSplashScreen);
+        imagesplash = (ImageView) findViewById(R.id.logoSplash);
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.change_animation);
+        imagesplash.startAnimation(anim);
+        textsplash.startAnimation(anim);
+        final Intent i = new Intent(this, LlistaProductesActivity.class);
+        Thread t= new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally{
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        t.start();
     }
 }
